2aee4ea1fa6119a9fd0f923b8ef42c69fa61ce35
Yyash64 <karanyash2@gmail.com>
2026-04-06 12:43:57 +0530

    fixed useGetCity hook

--- vingo/frontend/hooks/useGetCity.jsx
+++ vingo/frontend/hooks/useGetCity.jsx
@@ -1,23 +1,43 @@
-import React from "react";
 import { useEffect } from "react";
 import axios from "axios";
 import { setUserCity } from "./../redux/userSlice";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function useGetCity() {
   const dispatch = useDispatch();
   const apikey = import.meta.env.VITE_GEOAPI_KEY;
   const userData = useSelector((state) => state.user.userData);
+
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(async (position) => {
-      const { latitude, longitude } = position.coords;
-      const result = await axios.get(
-        `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&apiKey=${apikey}`,
-      );
-      dispatch(setUserCity(result?.data?.features[0].properties.city));
-    });
-  }, [userData, dispatch, apikey]);
+    if (!navigator.geolocation) {
+      console.warn("Geolocation is not supported");
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        try {
+          const { latitude, longitude } = position.coords;
+
+          const result = await axios.get(
+            `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&apiKey=${apikey}`
+          );
+
+          const city = result?.data?.features?.[0]?.properties?.city;
+
+          if (city) {
+            dispatch(setUserCity(city));
+          }
+        } catch (error) {
+          console.error("Failed to fetch city:", error);
+        }
+      },
+      (error) => {
+        console.error("Geolocation error:", error.message);
+      }
+    );
+  }, [dispatch, apikey]); 
+
 }
 
 export default useGetCity;
