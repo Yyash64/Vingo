14f761e4f899537e870723327bbbd6460fc4fbc9
Yyash64 <karanyash2@gmail.com>
2026-04-06 13:28:22 +0530

    set uup multer and cloudinary

---
+++ vingo/backend/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (file) => {
+  try {
+    const result = await cloudinary.uploader.upload(file);
+
+    if (fs.existsSync(file)) {
+      fs.unlinkSync(file);
+    }
+
+    return result.secure_url;
+  } catch (error) {
+    if (fs.existsSync(file)) {
+      fs.unlinkSync(file);
+    }
+    console.error("Cloudinary upload error:", error);
+  }
+};
+
+export default uploadOnCloudinary;
